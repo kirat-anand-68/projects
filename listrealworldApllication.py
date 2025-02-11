a56c94291155574e5b7733fbf3b746af6fd25600
Kirat Anand <167013352+kirat-anand-68@users.noreply.github.com>
2025-02-11 17:36:15 +0530

    Create listrealworldApllication.py

---
+++ listrealworldApllication.py
@@ -0,0 +1,24 @@
+# to_do_list=["TAke BAth","Go to College","Study Hard","Revise the Concept"]
+# to_do_list.append("Take Nap")
+# to_do_list.append("Make the Skill set String")
+
+# to_do_list.remove("Take Nap")
+
+# if "Study Hard" not in to_do_list:
+#     print("Do study hard and make the shine like the Sun")
+# else:
+#     print("Focus on study")
+# print("to_do_list remaining task")
+# for task in to_do_list:
+#     print(f"{task}")
+
+#GRADES OF THE SUDENTS:
+grades=[89,78,99,78,65]
+avg_grade=sum(grades)/len(grades)
+
+max_grade=max(grades)
+min_grade=min(grades)
+print(f" maximum grade is:{max_grade}")
+print(f" minimum grade is : {min_grade}")
+print(avg_grade)
+
